dbd7a09b813dbd21709b64e7002db0c02949c2bb
goldsteynAT <danny.093@outlook.com>
2025-03-01 14:24:26 +0100

    add genre, constructor for genre, getter method and dummy-data for initializeMovies

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -6,12 +6,15 @@ import java.util.List;
 public class Movie {
     private String title;
     private String description;
+    private List<Genre> genres;
     // TODO add more properties here
 
-    public Movie(String title, String description) {
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres = genres;
     }
+
 
     public String getTitle() {
         return title;
@@ -23,8 +26,22 @@ public class Movie {
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
-        // TODO add some dummy data here
+        movies.add(new Movie("Inception", "A thief who enters dreams to steal secrets.",
+                List.of(Genre.ACTION, Genre.SCIENCE_FICTION, Genre.THRILLER)));
+
+        movies.add(new Movie("The Godfather", "The aging patriarch of an organized crime dynasty transfers control.",
+                List.of(Genre.DRAMA, Genre.CRIME)));
+
+        movies.add(new Movie("Interstellar", "A team of explorers travel through a wormhole in space.",
+                List.of(Genre.SCIENCE_FICTION, Genre.DRAMA, Genre.ADVENTURE)));
+
 
         return movies;
     }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+
 }
